42b0bd9bc170f14597c1b755ec25ec79e33ecfef
dayanita18 <162450099+dayanita18@users.noreply.github.com>
2024-03-27 10:34:11 -0300

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,14 @@
+let nombre = "enrique"
+
+let apellido = "olguin"
+
+let edad = 20
+
+let verdadero = true
+
+let arreglo = ["enrique" , "olguin" , 20]
+
+let objeto = {nombre: "enrique", apellido : "olguin", edad : 20, verdadero : true}
+
+
+console.log(objeto)
